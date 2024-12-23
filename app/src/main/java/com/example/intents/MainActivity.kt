75ed78f496f5039501c8d26491995899e8ae295a
VK.muslapuram <vk.muslapuram@outlook.com>
2024-12-23 23:30:08 +0000

    Full Implementation

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -4,11 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,11 +25,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+
+            if(result.resultCode == RESULT_OK){
+                val replydata = result.data?.getStringExtra("replykey")?: "No Reply Recieved"
+                Snackbar.make(this, findViewById(R.id.main), replydata, Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
 
     fun giveSurprise(view: View) {
+        val til = findViewById<TextInputLayout>(R.id.textInputLayout)
+        val name = til.editText?.text.toString()
+
+//        val bundle = Bundle()
+//        bundle.putString("name", name)
+
         val i = Intent(this, Second_Activity::class.java)
-        startActivity(i)
+        i.putExtra("name", name)
+//        startActivity(i)
+        resultLauncher.launch(i)
     }
 
 }
--- app/src/main/java/com/example/intents/Second_Activity.kt
+++ app/src/main/java/com/example/intents/Second_Activity.kt
@@ -1,6 +1,8 @@
 package com.example.intents
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,17 @@ class Second_Activity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val name = intent.getStringExtra("name")
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = name
+
+    }
+
+    fun thankYou(view: View) {
+        val i = intent
+        i.putExtra("replykey", "Thank You Vijay !!")
+        setResult(RESULT_OK, i)
+        finish()
     }
 }
